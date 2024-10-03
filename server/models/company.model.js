e821921f66702e7bf21b648bc24d5a5ad8d664a6
Suyan Man Amatya <srkamatya007@gmail.com>
2024-10-03 13:14:53 +0545

    backend touch

---
+++ server/models/company.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    website: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    logo: {
+      type: String, // URl to company Logo
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
