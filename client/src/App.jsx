f432e1b7f513fb92a220dafb13968d0df1aa5835
Suyan Man Amatya <srkamatya007@gmail.com>
2024-10-04 18:18:02 +0545

    Backend finish frontend touch

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-400">iJobs</h1>
+      <Button />
+    </>
+  );
+}
+
+export default App;
