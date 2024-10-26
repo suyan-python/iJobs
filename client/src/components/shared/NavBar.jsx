52b8e553a89e394509371b6865341d8dfa0d9f3c
Suyan Man Amatya <srkamatya007@gmail.com>
2024-10-26 22:27:52 +0545

    Feature: Logout

--- client/src/components/shared/NavBar.jsx
+++ client/src/components/shared/NavBar.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Button } from "../ui/button";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { LogOut, User2 } from "lucide-react";
 import { Menu, X } from "lucide-react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "sonner";
+import { USER_API_END_POINT } from "@/utils/constant";
+import axios from "axios";
+import { setUser } from "@/redux/authSlice";
 
 const NavBar = () => {
   const { user } = useSelector((store) => store.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
@@ -17,6 +23,22 @@ const NavBar = () => {
 
   const activeClassName = "text-[#40A8F5]"; // Class for active link
   const inactiveClassName = "text-white hover:text-[#40A8F5] transition";
+
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get(`${USER_API_END_POINT}/logout`, {
+        withCredentials: true,
+      });
+      if (res.data.success) {
+        dispatch(setUser(null));
+        navigate("/");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.response.data.message);
+    }
+  };
 
   return (
     <div className="bg-black bg-opacity-30 backdrop-blur-md shadow-md w-full fixed top-0 left-0 z-50">
@@ -125,7 +147,11 @@ const NavBar = () => {
                   </div>
                   <div className="flex items-center">
                     <LogOut className="text-white" />
-                    <Button variant="link" className="text-[#40A8F5]">
+                    <Button
+                      onClick={logoutHandler}
+                      variant="link"
+                      className="text-[#40A8F5]"
+                    >
                       Logout
                     </Button>
                   </div>
@@ -218,7 +244,11 @@ const NavBar = () => {
                     </div>
                     <div className="flex items-center">
                       <LogOut className="text-white" />
-                      <Button variant="link" className="text-[#40A8F5]">
+                      <Button
+                        onClick={logoutHandler}
+                        variant="link"
+                        className="text-[#40A8F5]"
+                      >
                         Logout
                       </Button>
                     </div>
