4de09e35a66a045e6837f91b4c7b2397ebb52f34
Suyan Man Amatya <srkamatya007@gmail.com>
2024-11-12 00:55:48 +0545

    Production Lever Application

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,5 +1,7 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const USER_API_END_POINT =
+  "https://ijobs-1-vf05.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://ijobs-1-vf05.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+  "https://ijobs-1-vf05.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://ijobs-1-vf05.onrender.com/api/v1/company";
