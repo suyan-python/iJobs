35862ac0614499606ddbe2735e31d667db0b06a1
Suyan Man Amatya <srkamatya007@gmail.com>
2024-11-12 00:19:55 +0545

    V2: server client connection through packagejson

--- server/index.js
+++ server/index.js
@@ -7,10 +7,12 @@ import userRoute from "./routes/user.routes.js";
 import companyRoute from "./routes/company.routes.js";
 import jobRoute from "./routes/job.routes.js";
 import applicationRoute from "./routes/application.routes.js";
+import path from "path";
 
 dotenv.config({});
 
 const app = express();
+const _dirname = path.resolve();
 
 //middleware
 app.use(express.json());
@@ -29,6 +31,12 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+
+app.use(express.static(path.join(_dirname, "/client/dist")));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connectDB();
