61bfbc91e824a45e65139f60fa1377a4c5236c86
Suyan Man Amatya <srkamatya007@gmail.com>
2024-11-12 11:00:42 +0545

    Production: Url Connection frontent and backend

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://ijobs-1-vf05.onrender.com"],
   credentials: true,
 };
 
