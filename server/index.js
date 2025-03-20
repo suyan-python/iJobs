e3872469318f36e084a0f8c36d78963732064654
Suyan Man Amatya <srkamatya007@gmail.com>
2025-03-20 15:30:31 +0545

    production level final push

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: ["http://localhost:8000"],
+  origin: ["https://ijobs-1-vf05.onrender.com"],
   credentials: true,
 };
 
